0a7b6ce9d9ba558389a95a0ba312cd08c76de601
syntheticmagus <33846034+syntheticmagus@users.noreply.github.com>
2022-04-25 10:56:48 -0700

    Refactors and tweaks to improve sliding behavior.

--- app_package/src/firstPersonPlayer.ts
+++ app_package/src/firstPersonPlayer.ts
@@ -1,5 +1,6 @@
+import { IPhysicsEngine } from "@babylonjs/core";
 import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
-import { Matrix, Quaternion, Vector3 } from "@babylonjs/core/Maths/math";
+import { Matrix, Quaternion, TmpVectors, Vector3 } from "@babylonjs/core/Maths/math";
 import { Mesh } from "@babylonjs/core/Meshes/mesh";
 import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
 import "@babylonjs/core/Misc/observableCoroutine";
@@ -9,51 +10,52 @@ import { InputSampler, InputSamplerAxis } from "./inputSampler";
 
 export class FirstPersonPlayer {
     private readonly _scene: Scene;
+    private readonly _physicsEngine: IPhysicsEngine;
+
     private readonly _camera: FreeCamera;
-    private readonly _capsule: Mesh;
+    private readonly _collision: Mesh;
 
     public constructor(scene: Scene) {
         this._scene = scene;
+        this._physicsEngine = scene.getPhysicsEngine()!;
         
-        this._capsule = MeshBuilder.CreateCapsule("player", { radius: 0.25, height: 1.7 }, scene);
-        this._capsule.position.y = 1;
-        this._capsule.physicsImpostor = new PhysicsImpostor(this._capsule, PhysicsImpostor.SphereImpostor, { mass: 10, restitution: 0, friction: 10000 }, scene);
+        this._collision = MeshBuilder.CreateSphere("player", { segments: 3, diameterX: 0.4, diameterY: 1.7, diameterZ: 0.4 }, scene);
+        this._collision.position.y = 1;
+        this._collision.physicsImpostor = new PhysicsImpostor(this._collision, PhysicsImpostor.SphereImpostor, { mass: 10, restitution: 0, friction: 10000 }, scene);
         {
-            const angularFactor = this._capsule.physicsImpostor.physicsBody.getAngularFactor();
+            const angularFactor = this._collision.physicsImpostor.physicsBody.getAngularFactor();
             angularFactor.setX(0);
             angularFactor.setY(0);
             angularFactor.setZ(0);
         }
 
         this._camera = new FreeCamera("firstPersonPlayerCamera", new Vector3(), scene);
-        this._camera.minZ = 0.05;
+        this._camera.minZ = 0.01;
         this._camera.maxZ = 100;
         this._camera.position.y = 0.5
-        this._camera.parent = this._capsule;
+        this._camera.parent = this._collision;
 
         this._scene.onBeforeRenderObservable.runCoroutineAsync(this._perFrameUpdateCoroutine());
     }
-
+    
     private *_perFrameUpdateCoroutine() {
-        const right = new Vector3();
-        const forward = new Vector3();
-        const movement = new Vector3();
-        const raycastFrom = new Vector3();
-        const raycastTo = new Vector3();
+        const right = TmpVectors.Vector3[0];
+        const forward = TmpVectors.Vector3[1];
+        const movement = TmpVectors.Vector3[2];
+        const raycastFrom = TmpVectors.Vector3[0];
+        const raycastTo = TmpVectors.Vector3[1];
         const floorRotationQuaternion = new Quaternion();
         const floorRotationTransform = new Matrix();
         let m: Matrix;
 
         const input = new InputSampler(this._scene.getEngine());
-        const physicsEngine = this._scene.getPhysicsEngine()!;
         while (true) {
             const SLIDE_THRESHOLD = Math.PI / 3;
 
-            raycastFrom.copyFrom(this._capsule.position);
-            raycastFrom.y -= 0.8;
-            raycastTo.copyFrom(this._capsule.position);
-            raycastTo.y -= 1.2;
-            const raycastResult = physicsEngine.raycast(this._capsule.position, this._capsule.position.subtract(Vector3.Up().scale(1.1)));
+            raycastFrom.copyFrom(this._collision.position);
+            raycastTo.copyFrom(this._collision.position);
+            raycastTo.y -= 1;
+            const raycastResult = this._physicsEngine.raycast(raycastFrom, raycastTo);
 
             floorRotationTransform.copyFrom(Matrix.IdentityReadOnly);
             const floorAngle = raycastResult.hasHit ? Math.acos(Vector3.Dot(Vector3.UpReadOnly, raycastResult.hitNormalWorld)) : 0;
@@ -64,27 +66,28 @@ export class FirstPersonPlayer {
                 floorRotationQuaternion.toRotationMatrix(floorRotationTransform);
             }
 
-            if (floorAngle < SLIDE_THRESHOLD) {
-                this._capsule.physicsImpostor!.friction = 10000;
-                m = this._camera.getWorldMatrix();
-                right.copyFromFloats(m.m[0], m.m[1], m.m[2]);
-                forward.copyFromFloats(m.m[8], m.m[9], m.m[10]);
-                right.y = 0;
-                right.normalize();
-                forward.y = 0;
-                forward.normalize();
-                movement.set(0, 0, 0);
-                forward.scaleAndAddToRef(input.get(InputSamplerAxis.W) - input.get(InputSamplerAxis.S), movement);
-                right.scaleAndAddToRef(input.get(InputSamplerAxis.D) - input.get(InputSamplerAxis.A), movement);
-                movement.normalize();
-                Vector3.TransformNormalToRef(movement, floorRotationTransform, movement);
-                movement.scaleAndAddToRef(0.08 + 0.08 * input.get(InputSamplerAxis.Shift), this._capsule.position);
+            m = this._camera.getWorldMatrix();
+            right.copyFromFloats(m.m[0], m.m[1], m.m[2]);
+            forward.copyFromFloats(m.m[8], m.m[9], m.m[10]);
+            right.y = 0;
+            right.normalize();
+            forward.y = 0;
+            forward.normalize();
+            movement.set(0, 0, 0);
+            forward.scaleAndAddToRef(input.get(InputSamplerAxis.W) - input.get(InputSamplerAxis.S), movement);
+            right.scaleAndAddToRef(input.get(InputSamplerAxis.D) - input.get(InputSamplerAxis.A), movement);
+            movement.normalize();
+            Vector3.TransformNormalToRef(movement, floorRotationTransform, movement);
+            movement.scaleAndAddToRef(0.08 + 0.08 * input.get(InputSamplerAxis.Shift), this._collision.position);
 
-                if (raycastResult.hasHit && input.get(InputSamplerAxis.Space) > 0) {
-                    this._capsule.physicsImpostor!.applyImpulse(Vector3.UpReadOnly.scale(10), Vector3.ZeroReadOnly);
+            if (floorAngle < SLIDE_THRESHOLD) {
+                this._collision.physicsImpostor!.friction = 10000;
+
+                if (raycastResult.hasHit && raycastResult.hitDistance < 0.9 && input.get(InputSamplerAxis.Space) > 0) {
+                    this._collision.physicsImpostor!.applyImpulse(Vector3.UpReadOnly.scale(10), Vector3.ZeroReadOnly);
                 }
             } else {
-                this._capsule.physicsImpostor!.friction = 0;
+                this._collision.physicsImpostor!.friction = 0;
             }
 
             this._camera.rotation.y += input.get(InputSamplerAxis.MouseDY) / 200;
